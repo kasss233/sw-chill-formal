a8091eabd88293606540813a997e200714f593b3
kasssssssssss <1592858973@qq.com>
2025-12-05 20:08:49 +0800

    [add] 打字动画

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -1,6 +1,13 @@
 extends Node3D
 @export var emotion_animation:AnimationPlayer
 @export var action_animation:AnimationPlayer
-enum ActionState{
-    STANDING,SITTING,LYING
-}
+@export var face:MeshInstance3D
+func saying():
+	emotion_animation.play("saying")
+func typing():
+	action_animation.play("Sitting/typing")
+func reset_emotion():
+	emotion_animation.play("RESET")
+func stop_action():
+	action_animation.stop()
+	
