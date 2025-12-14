032c60fc74b5612b771874a55a9878ef835a1370
kasssssssssss <1592858973@qq.com>
2025-12-14 19:56:19 +0800

    [wip] 3d场景修改

--- scenes/main/ui/music_module/music_module.gd
+++ scenes/main/ui/music_module/music_module.gd
@@ -2,13 +2,34 @@ extends Control
 @onready var vbox=$FoldableContainer/ScrollContainer/VBoxContainer
 @onready var folder=$FoldableContainer
 signal music_changed(_name:String)
-func get_signal():
+signal music_changed_play_status
+signal music_changed_to_next
+signal music_changed_to_last
+func _ready() -> void:
+	get_signal()
+
+func get_signal() -> void:
 	for c in vbox.get_children():
-		c= c as SingleMusic
-		c.music_changed.connect(Callable(self,"change_music"))
-		
-func change_music(_name:String):
-	emit_signal("music_changed",_name)
-	folder.title=_name
-	
-	
+		c = c as SingleMusic
+		if c:
+			c.music_changed.connect(change_music)
+
+func change_music(_name:String) -> void:
+	print("[%s] change_music" % name)
+	music_changed.emit(_name)
+	folder.title = _name
+
+
+func _on_last_button_pressed() -> void:
+	music_changed_to_last.emit()
+	print("[%s] music_changed_to_last emitted" % name)
+
+
+func _on_status_button_pressed() -> void:
+	music_changed_play_status.emit()
+	print("[%s] music_changed_play_status emitted" % name)
+
+
+func _on_next_button_pressed() -> void:
+	music_changed_to_next.emit()
+	print("[%s] music_changed_to_next emitted" % name)
--- scenes/main/ui/music_module/single_music/single_music.gd
+++ scenes/main/ui/music_module/single_music/single_music.gd
@@ -1,8 +1,10 @@
 class_name SingleMusic
 extends Control
-
+@export var music_name:String="null"
 @onready var button=$Button
 signal music_changed(_name:String)
+func _ready() -> void:
+	set_music_name(music_name)
 func set_music_name(_name:String):
 	button.text=_name
 func get_music_name():
