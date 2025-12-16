e92660314d84599096f8036b806be94d440fa30c
kasss233 <1592858973@qq.com>
2025-12-16 19:39:26 +0800

    [wip] music逻辑完善

--- scenes/main/ui/music_module/music_module.gd
+++ scenes/main/ui/music_module/music_module.gd
@@ -3,26 +3,26 @@ extends Control
 @onready var folder=$FoldableContainer
 signal music_changed(_name:String)
 signal music_changed_play_status
-signal music_changed_to_next
-signal music_changed_to_last
+var cur_index:int
 func _ready() -> void:
 	get_signal()
-
 func get_signal() -> void:
 	for c in vbox.get_children():
 		c = c as SingleMusic
 		if c:
-			c.music_changed.connect(change_music)
+			c.music_changed.connect(change_music.bind(c))
 
-func change_music(_name:String) -> void:
-	print("[%s] change_music" % name)
+func change_music(_name:String,_emitter:SingleMusic) -> void:
+	print("[%s] change_music,index:[%d]" % [_emitter.name,_emitter.get_index()])
 	music_changed.emit(_name)
 	folder.title = _name
+	cur_index = _emitter.get_index()
 
 
 func _on_last_button_pressed() -> void:
-	music_changed_to_last.emit()
-	print("[%s] music_changed_to_last emitted" % name)
+	var last_index=(cur_index-1+vbox.get_child_count())%vbox.get_child_count()
+	var last_music=vbox.get_child(last_index) as SingleMusic
+	last_music.play_music()
 
 
 func _on_status_button_pressed() -> void:
@@ -31,5 +31,6 @@ func _on_status_button_pressed() -> void:
 
 
 func _on_next_button_pressed() -> void:
-	music_changed_to_next.emit()
-	print("[%s] music_changed_to_next emitted" % name)
+	var next_index=(cur_index+1)%vbox.get_child_count()
+	var next_music=vbox.get_child(next_index) as SingleMusic
+	next_music.play_music()
--- scenes/main/ui/music_module/single_music/single_music.gd
+++ scenes/main/ui/music_module/single_music/single_music.gd
@@ -1,7 +1,7 @@
 class_name SingleMusic
 extends Control
 @export var music_name:String="null"
-@onready var button=$Button
+@onready var button=$HBoxContainer/Button
 signal music_changed(_name:String)
 func _ready() -> void:
 	set_music_name(music_name)
@@ -9,5 +9,7 @@ func set_music_name(_name:String):
 	button.text=_name
 func get_music_name():
 	return button.text
+func play_music():
+	emit_signal("music_changed",button.text)
 func _on_button_pressed() -> void:
 	emit_signal("music_changed",button.text)
