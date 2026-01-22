937063fb2d8c7327dbba8df5ff754ec3873ba4a0
kasssssssssss <1592858973@qq.com>
2026-01-22 15:06:56 +0800

    [add] notebook逻辑与ui完善

--- scenes/main/ui/notebook_module/note_book.gd
+++ scenes/main/ui/notebook_module/note_book.gd
@@ -3,35 +3,60 @@ extends Control
 @export var page_button: PackedScene
 ## 页面内容
 @export var page: PackedScene
-@onready var vbox = $VBoxContainer
-@onready var inner_panel = $InnerPanel
+@export_category("子节点")
+@export var vbox: VBoxContainer
+@export var inner_panel: InnerPanel
+@export var panel: FrostedPanel
 ## 数组变量，保存pagebutton名称
 var page_names: Array[String] = []
-## 保存页面名称和对应的页面内容
+## 保存页面按钮和对应的页面内容
 var pages: Dictionary = {}
-var current_page_name: String = ""
+var current_page_btn: PageButton = null
 var is_page_opened: bool = false
 func _on_add_button_pressed() -> void:
 	var new_page_button = page_button.instantiate() as PageButton
 	vbox.add_child(new_page_button)
-	new_page_button.set_page_name("新页面" + str(page_names.size() + 1))
-	page_names.append(new_page_button.get_page_name())
-	new_page_button.page_changed.connect(_on_page_changed)
 	
-func _on_page_changed(_name: String) -> void:
-	if _name == current_page_name:
+	var base_name = "新页面"
+	var new_name = base_name
+	var counter = 1
+	while page_names.has(new_name):
+		new_name = base_name + str(counter)
+		counter += 1
+		
+	new_page_button.set_page_name(new_name)
+	page_names.append(new_page_button.get_page_name())
+	new_page_button.page_changed.connect(_on_page_changed.bind(new_page_button))
+	new_page_button.page_removed.connect(_on_page_removed.bind(new_page_button))
+func _on_page_changed(_name: String, btn: PageButton) -> void:
+	if btn == current_page_btn:
 		return
 	if is_page_opened:
 		# 保存当前页面内容
 		var current_page = inner_panel.get_child(0) as Page
-		pages[current_page_name] = current_page.get_text()
+		pages[current_page_btn] = current_page.get_text()
 		inner_panel.remove_child(current_page)
-	current_page_name = _name
+	current_page_btn = btn
 	var new_page = page.instantiate() as Page
 	inner_panel.add_child(new_page)
 	is_page_opened = true
 	# 如果之前有保存的内容，加载它
-	if pages.has(_name):
-		new_page.set_text(pages[_name])
+	if pages.has(btn):
+		new_page.set_text(pages[btn])
 	else:
 		new_page.set_text("")
+func _on_page_removed(_name: String, btn: PageButton) -> void:
+	# 如果删除的是当前页面，清空内容区域
+	if btn == current_page_btn:
+		var current_page = inner_panel.get_child(0) as Page
+		inner_panel.remove_child(current_page)
+		is_page_opened = false
+		current_page_btn = null
+	# 从页面名称数组和内容字典中移除
+	page_names.erase(_name)
+	pages.erase(btn)
+	# 从VBoxContainer中移除对应的PageButton
+	vbox.remove_child(btn)
+	btn.queue_free()
+func _on_material_button_pressed() -> void:
+	panel.visible = not panel.visible
--- scenes/main/ui/notebook_module/pagebutton/page_button.gd
+++ scenes/main/ui/notebook_module/pagebutton/page_button.gd
@@ -1,12 +1,58 @@
 class_name PageButton
 extends Control
-@onready var button = $MaterialButton
+@onready var line_edit = $InnerPanel/HBoxContainer/LineEdit
+@onready var remove_button = $InnerPanel/HBoxContainer/RemoveButton
+@onready var inner_panel = $InnerPanel
 signal page_changed(_name: String)
+signal page_removed(_name: String)
 var page_name: String = " "
+
+
 func set_page_name(_name: String):
 	page_name = _name
-	button.text = page_name
+	line_edit.text = _name
 func get_page_name() -> String:
 	return page_name
-func _on_material_button_pressed() -> void:
-	page_changed.emit(page_name)
+
+func _on_line_edit_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if event.double_click:
+			line_edit.editable = true
+			line_edit.grab_focus()
+			line_edit.select_all()
+		else:
+			page_changed.emit(page_name)
+
+func _on_line_edit_focus_exited() -> void:
+	line_edit.editable = false
+	page_name = line_edit.text
+
+func _on_line_edit_text_submitted(_new_text: String) -> void:
+	line_edit.editable = false
+	line_edit.release_focus()
+
+func _on_inner_panel_mouse_entered() -> void:
+	remove_button.show()
+
+func _on_inner_panel_mouse_exited() -> void:
+	remove_button.hide()
+
+
+func _on_line_edit_mouse_entered() -> void:
+	remove_button.show()
+
+
+func _on_line_edit_mouse_exited() -> void:
+	remove_button.hide()
+
+
+func _on_remove_button_mouse_entered() -> void:
+	remove_button.show()
+
+
+func _on_remove_button_mouse_exited() -> void:
+	remove_button.hide()
+
+
+func _on_remove_button_pressed() -> void:
+	page_removed.emit(page_name)
