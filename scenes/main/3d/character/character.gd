2a6683f730a60019e4900315ad4503296199989d
kasssssssssss <1592858973@qq.com>
2026-02-11 16:53:26 +0800

    [wip] 角色动画脚本重构

--- scenes/main/3d/character/character.gd
+++ scenes/main/3d/character/character.gd
@@ -1,70 +1,167 @@
+@tool
 class_name Character
 extends Node3D
-@export var emotion_animation: AnimationPlayer
-@export var action_animation: AnimationPlayer
 @export var action_tree: AnimationTree
 @export var emotion_tree: AnimationTree
-@export var typing: bool = false
-@export var happy: bool = false
-@export var saying: bool = false
-@export var sad: bool = true
-@export var surprised: bool = true
-@export var angry: bool = true
-var action_playback: AnimationNodeStateMachinePlayback
-func _ready() -> void:
-	_sanitize_animations(emotion_animation)
-	_sanitize_animations(action_animation)
-	action_tree.active = true
-	emotion_tree.active = true
-	action_playback = action_tree.get("parameters/playback")
 
-func _sanitize_animations(player: AnimationPlayer):
-	if not player: return
-	var library_names = player.get_animation_library_list()
-	for lib_name in library_names:
-		var library = player.get_animation_library(lib_name)
-		for anim_name in library.get_animation_list():
-			var anim = library.get_animation(anim_name)
-			for i in range(anim.get_track_count() - 1, -1, -1):
-				if anim.track_get_type(i) == Animation.TYPE_BLEND_SHAPE:
-					var path = anim.track_get_path(i)
-					var node = player.get_node_or_null(path)
-					if node is MeshInstance3D:
-						if node.mesh == null or node.mesh.get_blend_shape_count() == 0:
-							anim.remove_track(i)
-						else:
-							# Optional: check if index is out of bounds
-							# But usually size=0 is the issue here.
-							pass
-## 对话接口
-func start_saying():
-	saying = true
-func stop_saying():
-	saying = false
+## 编辑器测试工具
+enum ActionTest {
+	IDLE,
+	TYPING,
+	NOT_TYPING,
+	CLAP,
+	THINK,
+	CHEER,
+	WATCH,
+	GREET,
+	SURPRISED,
+	DISBELIEF,
+	STRETCH,
+	STRETCH2
+}
+
+enum EmotionTest {
+	NEUTRAL,
+	HAPPY,
+	SAD,
+	SURPRISED,
+	ANGRY,
+	SAYING,
+	BLINKING
+}
+
+@export var test_action: ActionTest = ActionTest.IDLE:
+	set(value):
+		test_action = value
+		if Engine.is_editor_hint():
+			_test_action(value)
+
+@export var test_emotion: EmotionTest = EmotionTest.NEUTRAL:
+	set(value):
+		test_emotion = value
+		if Engine.is_editor_hint():
+			_test_emotion(value)
+var action_playback: AnimationNodeStateMachinePlayback
+var emotion_playback: AnimationNodeStateMachinePlayback
+func _ready() -> void:
+	if action_tree:
+		action_tree.active = true
+		action_playback = action_tree.get("parameters/playback")
+	if emotion_tree:
+		emotion_tree.active = true
+		emotion_playback = emotion_tree.get("parameters/playback")
 	
 ## 动作接口
-func set_typing_pose(_typing: bool):
-	typing = _typing
-func set_watch_pose():
-	action_playback.travel("pose_1")
-func set_cheer_pose():
-	action_playback.travel("cheer")
-func set_disbelief_pose():
-	action_playback.travel("disbelief")
-func set_dodge_pose():
-	action_playback.travel("dodge")
-func set_angry_pose():
-	action_playback.travel("angry")
+func set_idle_pose():
+	if action_playback:
+		action_playback.travel("idle")
+## typing一直循环，需要手动跳转到idle
+func set_typing_pose():
+	if action_playback:
+		action_playback.travel("typing")
+## 以下动作都是一次性动作，播放完会自动回到idle
 func set_clap_pose():
-	action_playback.travel("clap")
-func set_laughing_pose():
-	action_playback.travel("laughing")
+	if action_playback:
+		action_playback.travel("clap")
+func set_think_pose():
+	if action_playback:
+		action_playback.travel("think")
+func set_cheer_pose():
+	if action_playback:
+		action_playback.travel("cheer")
+func set_watch_pose():
+	if action_playback:
+		action_playback.travel("watch")
+func set_greet_pose():
+	if action_playback:
+		action_playback.travel("greet")
+func set_surprised_pose():
+	if action_playback:
+		action_playback.travel("surprised")
+func set_disbelief_pose():
+	if action_playback:
+		action_playback.travel("disbelief")
+func set_stretch_pose():
+	if action_playback:
+		action_playback.travel("stretch")
+func set_stretch2_pose():
+	if action_playback:
+		action_playback.travel("stretch2")
+
 ## 表情接口
-func set_happy(enable: bool):
-	happy = enable
-func set_sad(enable: bool):
-	sad = enable
-func set_surprised(enable: bool):
-	surprised = enable
-func set_angry(enable: bool):
-	angry = enable
+## blinking为一次性动作，播放完会自动回到neutral
+func set_blinking():
+	if emotion_playback:
+		emotion_playback.travel("blinking")
+## 以下表情都是持续表情，需要手动跳转到neutral
+func set_neutral():
+	if emotion_playback:
+		emotion_playback.travel("neutral")
+func set_happy():
+	if emotion_playback:
+		emotion_playback.travel("happy")
+func set_sad():
+	if emotion_playback:
+		emotion_playback.travel("sad")
+func set_surprised():
+	if emotion_playback:
+		emotion_playback.travel("surprised")
+func set_angry():
+	if emotion_playback:
+		emotion_playback.travel("angry")
+func set_saying():
+	if emotion_playback:
+		emotion_playback.travel("saying")
+
+## 编辑器测试辅助函数
+func _test_action(action: ActionTest):
+	if not action_playback:
+		_ready()
+	if not action_playback:
+		return
+	
+	match action:
+		ActionTest.IDLE:
+			set_idle_pose()
+		ActionTest.TYPING:
+			set_typing_pose()
+		ActionTest.CLAP:
+			set_clap_pose()
+		ActionTest.THINK:
+			set_think_pose()
+		ActionTest.CHEER:
+			set_cheer_pose()
+		ActionTest.WATCH:
+			set_watch_pose()
+		ActionTest.GREET:
+			set_greet_pose()
+		ActionTest.SURPRISED:
+			set_surprised_pose()
+		ActionTest.DISBELIEF:
+			set_disbelief_pose()
+		ActionTest.STRETCH:
+			set_stretch_pose()
+		ActionTest.STRETCH2:
+			set_stretch2_pose()
+
+func _test_emotion(emotion: EmotionTest):
+	if not emotion_playback:
+		_ready()
+	if not emotion_playback:
+		return
+	
+	match emotion:
+		EmotionTest.NEUTRAL:
+			set_neutral()
+		EmotionTest.HAPPY:
+			set_happy()
+		EmotionTest.SAD:
+			set_sad()
+		EmotionTest.SURPRISED:
+			set_surprised()
+		EmotionTest.ANGRY:
+			set_angry()
+		EmotionTest.SAYING:
+			set_saying()
+		EmotionTest.BLINKING:
+			set_blinking()
