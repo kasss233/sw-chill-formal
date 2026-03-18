f3cd1f9e669cc916c0d231cf6fce3e3e40b462f7
kasssssssssss <1592858973@qq.com>
2026-03-18 15:54:10 +0800

    [feat] 光照优化4

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -2,10 +2,10 @@ extends Node
 
 @export var ui: UI
 @export var ui_mobile: UI
-@export var main3d:Main3d
+@export var main3d: Main3d
 @export_category("项目演示")
 @export var animation_player: AnimationPlayer
-@export var snack_bar:MaterialSnackbar
+@export var snack_bar: MaterialSnackbar
 func _ready() -> void:
 	_init_ui()
 
@@ -13,10 +13,9 @@ func _ready() -> void:
 #这里根据设备切换ui，视口
 #TODO:判断手机pad
 func _init_ui():
-
 	if OS.has_feature("mobile"):
 	#if OS.has_feature("pc"):
-		get_window().size = Vector2i(648, 1152) 
+		get_window().size = Vector2i(648, 1152)
 		get_window().content_scale_size = Vector2i(648, 1152)
 		get_tree().root.content_scale_factor = 1.8
 		ui.queue_free()
@@ -28,5 +27,3 @@ func _init_ui():
 		ui.visible = true
 		main3d.set_camera_fov(45)
 		return
-	
-	
